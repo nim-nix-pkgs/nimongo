7b2b4cc82a99557ce5022b32245d31c37ecb42e4
hugosenari <hugosenari@gmail.com>
2022-03-12 04:30:05 -0300

    chore: re index nimongo

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''MongoDB driver in pure Nim language with synchronous and asynchronous I/O support'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimongo-master".url = "path:./master";
+  inputs."nimongo-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimongo-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimongo-v0_1_0".url = "path:./v0_1_0";
+  inputs."nimongo-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimongo-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimongo-v0_2_0".url = "path:./v0_2_0";
+  inputs."nimongo-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimongo-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''MongoDB driver in pure Nim language with synchronous and asynchronous I/O support'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimongo-master.flake = false;
+  inputs.src-nimongo-master.owner = "SSPkrolik";
+  inputs.src-nimongo-master.ref   = "refs/heads/master";
+  inputs.src-nimongo-master.repo  = "nimongo";
+  inputs.src-nimongo-master.type  = "github";
+  
+  inputs."scram".dir   = "nimpkgs/s/scram";
+  inputs."scram".owner = "riinr";
+  inputs."scram".ref   = "flake-pinning";
+  inputs."scram".repo  = "flake-nimble";
+  inputs."scram".type  = "github";
+  inputs."scram".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."scram".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimongo-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimongo-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
